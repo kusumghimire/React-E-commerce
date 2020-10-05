7fbdcb15a8d0b9957887fe92b63dec5535f7e3df
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-05 13:36:55 +0545

    title and imageUrl added to directory.component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 1
+        },
+        {
+          title: "hats",
+          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          id: 2
+        },
+        {
+          title: "hats",
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 3
+        },
+        {
+          title: "hats",
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 4
+        },
+        {
+          title: "hats",
+          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          id: 5
+        },
+      ]
+
+    }
+  }
+
+
+}
