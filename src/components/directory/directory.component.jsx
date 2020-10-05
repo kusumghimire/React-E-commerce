0e354bb6d3cb2c7a4ac26299c8bae8d0dbe7c8bf
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-05 13:43:46 +0545

    mapping section items into the render()

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,27 +12,26 @@ class Directory extends React.Component {
           id: 1
         },
         {
-          title: "hats",
+          title: "jackets",
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2
         },
+
         {
-          title: "hats",
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: "sneakers",
+          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3
         },
         {
-          title: "hats",
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: "womens",
+          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           id: 4
         },
         {
-          title: "hats",
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          title: "mens",
+          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           id: 5
-        },
-      ]
-
+        }]
     }
   }
 
