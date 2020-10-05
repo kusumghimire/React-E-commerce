0e9381f69ab8083cc66db376db577953c1308aa1
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-05 22:35:56 +0545

    Switch is imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from  './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
-const HatsPage= ()=>(
+const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
   </div>
@@ -14,8 +14,10 @@ const HatsPage= ()=>(
 function App() {
   return (
     <div >
-     <Route exact path='/' component={HomePage} />
-     <Route  path='/' component={HatsPage} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
