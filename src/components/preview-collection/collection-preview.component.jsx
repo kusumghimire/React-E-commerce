474fff13b413bd50aa5e1fe17f5eb672692acd98
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 18:48:57 +0545

    .collection-preview is created

---
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './preview-collection.styles.scss';
+
+const PreviewCollection = () => (
+  <div className='collection-preview'>
+    <h1>Title</h1>
+    <div className='preview'>
+
+    </div>
+  </div>
+)
