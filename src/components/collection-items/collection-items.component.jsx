a51cf35182d97bbfd20a2e668095c6a2e411b447
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 20:10:28 +0545

    .collection-item is created

---
+++ src/components/collection-items/collection-items.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './collection-items.styles.scss';
+
+const Collection =({id,name,price,imageUrl}) =>(
+    <div className="collection-item">
+        <div className="image">
+
+        </div>
+    </div>
+)
