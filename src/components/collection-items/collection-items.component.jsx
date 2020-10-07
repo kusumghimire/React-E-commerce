e3c3991af85a01b9b40e74a8ca6638a728de0899
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 20:13:32 +0545

    .collection-footer is created

--- src/components/collection-items/collection-items.component.jsx
+++ src/components/collection-items/collection-items.component.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './collection-items.styles.scss';
 
-const Collection =({id,name,price,imageUrl}) =>(
-    <div className="collection-item">
-        <div className="image">
+const Collection = ({ id, name, price, imageUrl }) => (
+  <div className="collection-item">
+    <div className="image"
+    style={{backgroundImage:`url(${imageUrl})`
+    }}
+    />
+      <div className="collection-footer">
 
-        </div>
     </div>
+  </div>
 )
