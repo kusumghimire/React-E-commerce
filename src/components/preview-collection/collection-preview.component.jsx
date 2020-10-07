2c5584566c4ba4a8d38867779bb2b8b0c987bb52
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 18:54:48 +0545

    maping is added to items

--- src/components/preview-collection/collection-preview.component.jsx
+++ src/components/preview-collection/collection-preview.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import './preview-collection.styles.scss';
 
-const PreviewCollection = () => (
+const PreviewCollection = ({ title, items }) => (
   <div className='collection-preview'>
-    <h1>Title</h1>
+    <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-
+      {
+        items.map(item => (
+          <div>{item.name}</div>
+        ))}
     </div>
   </div>
 )
