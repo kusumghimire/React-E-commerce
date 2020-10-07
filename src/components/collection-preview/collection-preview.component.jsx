922298844887015dca508eddb13e1968c92933eb
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 20:07:18 +0545

    collection-item folder is created

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,7 +8,8 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview'>
       {
         items
-          .filter((item, idx) => idx < 4).map(item => (
+          .filter((item, idx) => idx < 4)
+          .map(item => (
             <div key={item.id}>{item.name}</div>
           ))}
     </div>
