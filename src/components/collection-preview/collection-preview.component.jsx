2fd2dd2ca6c32cdf7264fa3a173d9fd0206578f8
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 19:56:34 +0545

    filter of index and item done in collection-preview.component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,9 +7,10 @@ const CollectionPreview = ({ title, items }) => (
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
       {
-        items.map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+        items
+          .filter((item, idx) => idx < 4).map(item => (
+            <div key={item.id}>{item.name}</div>
+          ))}
     </div>
   </div>
 );
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -16,13 +16,15 @@ class ShopPage extends React.Component {
     const { collections } = this.state;
     return (
       <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+  )
+)}
 
-
-      </div>
+      </div>  
     )
   };
-
-
 }
 
 export default ShopPage;
