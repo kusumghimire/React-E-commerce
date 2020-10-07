5e96f3a668d4489de85a58f4aec1bba1c7efa9bf
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-07 20:16:49 +0545

    css added to  collection-item

--- src/components/collection-items/collection-items.component.jsx
+++ src/components/collection-items/collection-items.component.jsx
@@ -5,11 +5,15 @@ import './collection-items.styles.scss';
 const Collection = ({ id, name, price, imageUrl }) => (
   <div className="collection-item">
     <div className="image"
-    style={{backgroundImage:`url(${imageUrl})`
-    }}
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
     />
-      <div className="collection-footer">
-
+    <div className="collection-footer">
+      <div className="name">{name}</div>
+      <div className="price">{price}</div>
     </div>
   </div>
 )
+
+export default CollectionItem;
