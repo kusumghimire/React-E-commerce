cc305bc5033b19d8976855e28e444eb2937a1588
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-08 18:28:09 +0545

    functional component added to SignInAndSignUpPage

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,1 +1,12 @@
 import React from 'react';
+
+import './sign-in-and-sign-up.styles.scss'
+
+const SignInAndSignUpPage =()=>(
+    <div className="sign-in-and-sign-up">
+SIGN IN
+    </div>
+
+);
+
+export default SignInAndSignUpPage;
