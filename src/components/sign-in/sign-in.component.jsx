4173bca281aa7ed564eb9825b02390261ff5a23f
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-08 18:39:14 +0545

    form added

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,14 +2,28 @@ import React from 'react';
 
 import './sign-in.styles.scss';
 
-class SignIn extends React.Component{
-constructor(props){
-  super(props);
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-  this.state= {
-    email:'',
-    password:''
+    this.state = {
+      email: '',
+      password: ''
+    }
   }
-}
 
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password </span>
+      <form action="
+      ">
+        
+      </form>
+      
+      </div>
+    )
+  }
 }
