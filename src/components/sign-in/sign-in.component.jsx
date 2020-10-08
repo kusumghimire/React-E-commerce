960c057df134372585fbcd93e67f2649078d87bf
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-10-08 18:35:47 +0545

    state added to sign-in component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,5 +3,13 @@ import React from 'react';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{
+constructor(props){
+  super(props);
+
+  this.state= {
+    email:'',
+    password:''
+  }
+}
 
 }
