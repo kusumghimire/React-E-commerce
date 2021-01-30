8634ec2bf7bf98d8cefe8dc7218923d0abf5a2aa
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-01-30 22:26:52 +0545

    sign-up form is created

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,3 +3,40 @@ import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import {auth, createUserProfileDocument} from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+class SignUp extends React.Component{
+    constructor(){
+        super();
+
+        this.state={
+            display:'',
+            email:'',
+            password:'',
+            confirmPassword:''
+        }
+    }
+    render(){
+        const {displayName, email, password, confirmPassword}=this.state;
+        return(
+            <div className="sign-up">
+                <h2 className="title">
+                    I do not have a account
+                </h2>
+                <span>Sign Up with email and password</span>
+                <form  className="sign-up-form" onSubmit={this.handleSubmit}>
+                    <FormInput>
+                   type='text'
+                   name='displayName'
+                   value={displayName}
+                   onChange={this.handleChange}
+                   label='Display Name'
+                   required
+                    </FormInput>
+
+                </form>
+            </div>
+        )
+    }
+}
