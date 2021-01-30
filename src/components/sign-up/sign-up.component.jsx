d5d0ce31998929beccf4f0f706b2ba96d88cf20c
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-01-30 22:19:38 +0545

    createUserProfileDocument is imported from firebase.utils

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
+import {auth, createUserProfileDocument} from '../../firebase/firebase.utils';
