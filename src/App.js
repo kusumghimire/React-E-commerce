fe3655fce674361887a5f139520b6d23b5e5348d
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-01 21:26:26 +0545

    implemented firebase utils, included ability to store authenticated users into our firebase database

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
     constructor() {
@@ -62,8 +62,8 @@ class App extends React.Component {
             /> <
             Route path = '/signin'
             component = { SignInAndSignUpPage }
-            /> <
-            /Switch> <
+            /> < /
+            Switch > <
             /div>
         );
     }
