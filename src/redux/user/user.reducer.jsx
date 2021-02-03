95638e5580eaf63b478792ea0fffdf19306dd6c9
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-03 21:30:31 +0545

    switch(action.type ) with case is written in user.redux.jsx

--- src/redux/user/user.reducer.jsx
+++ src/redux/user/user.reducer.jsx
@@ -2,5 +2,15 @@ const INITIAL_STATE = {
     currentUser: null
 }
 const userReducer = (state = INITIAL_STATE, aciton) => {
-
+ switch (aciton.type){
+     case 'SET_CURRENT_USER':
+         return{
+             ...state,
+             currentUser: aciton.payload
+         }
+     default:
+         return state;
+  }
 }
+
+export default userReducer;
