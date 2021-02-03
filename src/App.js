638bda0470f878acf989e59e2832b4f22b898e5e
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-03 22:11:35 +0545

    setCurrentUser is imported in user.actions and  it remove constructor from the App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connet } from 'react-redux';
 
 import './App.css';
 
@@ -8,15 +9,9 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            currentUser: null
-        };
-    }
 
     unsubscribeFromAuth = null;
 
@@ -68,4 +63,8 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
