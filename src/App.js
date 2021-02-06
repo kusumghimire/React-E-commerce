79c7f176b284dba899d1e42d1d7bbd7460003a96
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-06 20:42:07 +0545

    user.types.js is created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import { connet } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -13,59 +13,71 @@ import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
 
-        unsubscribeFromAuth = null;
+    unsubscribeFromAuth = null;
 
-        componentDidMount() {
-            const { setCurrentUser } = this.props;
+    componentDidMount() {
+        const { setCurrentUser } = this.props;
 
-            this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-                if (userAuth) {
-                    const userRef = await createUserProfileDocument(userAuth);
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
 
-                    userRef.onSnapshot(snapShot => {
-                        setCurrentUser({
-                            id: snapShot.id,
-                            ...snapShot.data()
-                        });
+                userRef.onSnapshot(snapShot => {
+                    setCurrentUser({
+                        id: snapShot.id,
+                        ...snapShot.data()
                     });
-                }
+                });
+            }
 
-                setCurrentUser(userAuth);
-            });
-        }
-
-
-        componentWillUnmount() {
-            this.unsubscribeFromAuth();
-        }
-
-        render() {
-            return ( <
-                div >
-                <
-                Header / >
-                <
-                Switch / >
-
-                <
-                Route exact path = '/'
-                component = { HomePage }
-                /> <
-                Route path = '/shop'
-                component = { ShopPage }
-                /> <
-                Route path = '/signin'
-                component = { SignInAndSignUpPage }
-                /> < 
-                Switch / >
-
-            );
-        }
-
-
-        const mapDispatchToProps = dispatch => ({
-            setCurrentUser: user => dispatch(setCurrentUser(user));
+            setCurrentUser(userAuth);
         });
+    }
 
 
-        export default connect(null, mapDispatchToProps)(App);
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return ( <
+            div >
+            <
+            Header / >
+            <
+            Switch >
+            <
+            Route exact path = '/'
+            component = { HomePage }
+            /> <
+            Route path = '/shop'
+            component = { ShopPage }
+            /> <
+            Route exact path = '/signin'
+            render = {
+                () =>
+                this.props.currentUser ? ( <
+                    Redirect to = '/' / >
+                ) : ( <
+                    SignInAndSignUpPage / >
+                )
+            }
+            /> < /
+            Switch > <
+            /div>
+        );
+    }
+}
+
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+ import { UserActionType } from './user.types.js'
+
+ const INITIAL_STATE = {
+     currentUser: null
+ }
+ const userReducer = (state = INITIAL_STATE, aciton) => {
+     switch (aciton.type) {
+         case 'SET_CURRENT_USER':
+             return {
+                 ...state,
+                 currentUser: aciton.payload
+             }
+         default:
+             return state;
+     }
+ }
+
+ export default userReducer;
