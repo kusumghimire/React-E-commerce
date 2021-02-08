336a5ded7921ce61b663bc3be618a6a06b0cf573
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-08 22:17:24 +0545

    collection-items is imported in collection-preview.component.jsx

--- src/components/collection-items/collection-items.component.jsx
+++ src/components/collection-items/collection-items.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import {connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
+import {addItem} from '../../redux/cart/cart.actions';
 import './collection-items.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+  const {id,name,price,imageUrl}= item;
+  return(
   <div className="collection-item">
     <div className="image"
       style={{
@@ -13,8 +17,14 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <div className="name">{name}</div>
       <div className="price">{price}</div>
     </div>
-    <CustomButton inverted> Add to cart</CustomButton>
+    <CustomButton onClick={()=> addItem(item)}> Add to cart</CustomButton>
   </div>
-)
 
-export default CollectionItem;
+  )
+}
+
+const mapDispatchToProps = dispatch =>({
+  addItem: item=> dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,13 +6,11 @@ const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-      {
-        items
-          .filter((item, idx) => idx < 4)
-          .map(({id, ...otheritemProps}) => (
-            <CollectionItem key={id} {...otheritemProps}>
-            </CollectionItem>
-          ))}
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
 );
