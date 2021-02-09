74349ad305bf841263c9553f96fd742927b74826
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-09 22:24:12 +0545

    CartItem is imported in cart-dropdown.component.jsx

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
-
+import CartItem from '../cart-item/card-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown =() =>(
