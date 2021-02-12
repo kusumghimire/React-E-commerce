5affe7f0989837ea1d47bdcb01529a5d8f39316d
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-12 22:04:43 +0545

    selectCurrentUser is imported in App.js

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
 import {auth} from '../../firebase/firebase.utils';
 import  CartIcon from '../cart-icon/cart-icon.component.jsx';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component.jsx';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
+import {selectCurrentUser} from '../../redux/user/user.selectors';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 // import { connectAdvanced } from 'react-redux';
@@ -34,9 +38,9 @@ const Header = ({currentUser, hidden}) => (
   </div>
 );
 
-const mapStateToProps= ({user: {currentUser}, cart:{hidden}}) =>({
-  currentUser,
-  hidden
-})
+const mapStateToProps =createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
+});
 
 export default connect(mapStateToProps)(Header);
