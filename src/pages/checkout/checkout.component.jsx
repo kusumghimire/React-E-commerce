7d8fdf137e09430eb82d828488ea1410d5d8a2eb
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-13 22:49:59 +0545

    createStructuredSelector and cartItem is  imported in checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,37 +1,44 @@
 import React  from 'react';
+import {connect} from 'react-redux';
+import {createStrcutredSelector} from 'reselect';
 
-import './chekout.styles.scss';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
+
+import './checkout.styles.scss';
 
 const CheckoutPage = () =>(
     <div className="checkout-page">
         <div className="checkout-header">
             <div className="header-block">
                 <span>
-
+                   Product
                 </span>
             </div>
             <div className="header-block">
                 <span>
-                    
+                    Description
                 </span>
             </div>
             <div className="header-block">
                 <span>
-                    
+                    Quantity
                 </span>
             </div>
             <div className="header-block">
                 <span>
-                    
+                    Price 
                 </span>
             </div>
             <div className="header-block">
                 <span>
-                    
+                    Remove
                 </span>
             </div>
         </div>
     </div>
 );
+ const mapStateToProps = createStrcutredSelector({
+     cartIems: selectCartItems
+ });
 
 export default CheckoutPage;
