b9c3245a1e50f88155c0b436fd8d6ae76628c80a
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-15 21:38:44 +0545

    updated cart tou use reselector from reselector

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,9 +1,8 @@
 import React  from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-
 import {selectCartItems, selectCartTotal} from '../../redux/cart/cart.selectors';
-
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import './checkout.styles.scss';
 
 const CheckoutPage = ({cartItems,total }) =>(
@@ -36,10 +35,10 @@ const CheckoutPage = ({cartItems,total }) =>(
             </div>
         </div>
         {
-            cartItems.map(cartItem =>
-                cartItem.name
+            cartItems.map(cartItem =>(
+            <CheckoutItem key={cartItem.id} cartItem={cartItem} />
 
-            )
+                )  )
         }
         <div className="total">
             <span>TOTAL:${total}</span>
