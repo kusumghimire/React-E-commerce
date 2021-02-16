d52d12563b9079005bce3656a4cd9f0eb7006e81
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-16 21:50:47 +0545

    directoryReducer is imported into rootreducer

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directorytReducer from './directory/directory.reducer';
 
 export default combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    directory: directorytReducer
 });
