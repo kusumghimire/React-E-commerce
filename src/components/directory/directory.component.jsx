8ecdb9a0711d812e564cddf6464fa7d5cb47bc2a
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-16 22:37:59 +0545

    createDirectorySection is created into directory component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectDirectorySections  } from '../../redux/directory/directory.selectors';
 
 import MenuItem from '../menu-item/menu-item.component';
 
 import './directory.styles.scss';
 
-const Directory = ({sections}) => (
+const Directory = ({ sections }) => (
   <div className="directory-menu">
     {sections.map(({ id, ...otherSectionProps }) => (
         <MenuItem key={id} {...otherSectionProps} />
-      ))
-    }
+      ))}
   </div>
 );
 
-const mapStateToProps =({})
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
+
+
