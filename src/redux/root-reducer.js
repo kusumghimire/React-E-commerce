3b2bc8343137391af76166b344a0e4c11584a1cc
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-16 20:27:31 +0545

    persistReducer is imported into root-reducer.jsx

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-
+import {persistReducer} from 'redux-persist';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
