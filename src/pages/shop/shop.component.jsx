a04d37f792007863196e0cd3af9bede0d2c776d8
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:56:05 +0545

    collections is destuctured from shop.component.jsx

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
   <div className='shop-page'>
-    <CollectionsOverview  />
+    <Route exact path={} component={CollectionsOverview } />
   </div>
 )
-
 
 export default ShopPage;
