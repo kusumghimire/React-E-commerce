9d6abbe79558fd6f3b3f7f97138533ccc5bcdd34
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:51:00 +0545

    styles.css  updated

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 const ShopPage = ({ collections }) => (
   <div className='shop-page'>
-    <CollectionsOverview />
+    <CollectionsOverview  />
   </div>
 )
 
