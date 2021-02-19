0335ebc1748cdd9d40cd6baa0365ac3f6a62ec73
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:34:36 +0545

    map collections is moved from shop component

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -6,6 +6,8 @@ import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({collections}) =>(
     <div className="collections-overview">
-
+{collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
     </div>
 )
