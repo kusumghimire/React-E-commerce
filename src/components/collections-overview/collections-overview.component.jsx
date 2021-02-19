83fa2290a2fdcbd005bd47c116342158b418663f
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:37:07 +0545

    collections preview is imported to collection-overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { CollectionPreview } from '../collection-preview/collection-preview.component';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({collections}) =>(
+const CollectionsOverview = ({ collections }) => (
     <div className="collections-overview">
-{collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+        {collections.map(({ id, ...otherCollectionProps }) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
     </div>
-)
+);
