2c5e438efd881489c21eece6784c79c143252d3c
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:50:19 +0545

    shop.component and collections.overview is modified and collections-overview is imported

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-const ShopPage = ({collections})=>(
+const ShopPage = ({ collections }) => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+    <CollectionsOverview />
   </div>
 )
 
