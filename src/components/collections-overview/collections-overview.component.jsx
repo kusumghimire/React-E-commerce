307a41987f3a021868523014c922c9eca3ae074e
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 22:32:13 +0545

    collections-overview const is created

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
+
+import './collections-overview.styles.scss';
+
+const CollectionsOverview = ({collections}) =>(
+    <div className="collections-overview">
+
+    </div>
+)
