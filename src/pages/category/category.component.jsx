b18dd990ce46cbfa7d4116ddbefa09128a0e4fab
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-19 23:03:51 +0545

    category page is created

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-items/collection-items.component';
+
+import './category.styles.scss';
+
+const CategoryPage =() =>(
+<div className="category">
+    <h2>CATEGORY PAGE</h2>
+</div>
+);
+
+export default CategoryPage;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-const ShopPage = () => (
+
+const ShopPage = ({match}) => (
   <div className='shop-page'>
-    <Route exact path={} component={CollectionsOverview } />
+    <Route exact path={`${match.path}`} component={CollectionsOverview } />
   </div>
 )
 
