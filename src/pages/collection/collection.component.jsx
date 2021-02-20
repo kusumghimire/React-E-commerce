186804963f8d1d0075715900082048097b70df06
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-20 18:49:39 +0545

    category page is replaced by collections

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-items/collection-items.component';
+
+import './collection.styles.scss';
+
+const CollectionPage =({match}) =>{
+    return(
+<div className="collection-page">
+    <h2>Collection PAGE</h2>
+</div>
+)};
+
+export default CollectionPage;
