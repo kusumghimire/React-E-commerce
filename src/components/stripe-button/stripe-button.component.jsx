56a87ac611dfe2cb220c652195eb3aff3bae816f
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-22 16:48:38 +0545

    stripeCheckout is imported into stripe-button.component.jsx

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,2 +1,13 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton =({price})=>{
+const priceForStripe = price*100;
+const publishableKey ='pk_test_51INbtmEDV1OQTgdxgHzm7ueWWv7IcD9muKeX0zzmkelbghZjGHBMm0tCDVzp8bEgf0QX4AR6wOnXoU64fDEMP17K00ouxgUWja'
+
+return(
+    <StripeCheckout 
+    
+    />
+)
+}
