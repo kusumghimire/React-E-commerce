882409a8b9fde8a606b7b8c2f95eec0ffcbc261b
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-22 16:55:08 +0545

    label,billingAddress, shippingAddress and others added to stripe-button

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 
-const StripeCheckoutButton =({price})=>{
-const priceForStripe = price*100;
-const publishableKey ='pk_test_51INbtmEDV1OQTgdxgHzm7ueWWv7IcD9muKeX0zzmkelbghZjGHBMm0tCDVzp8bEgf0QX4AR6wOnXoU64fDEMP17K00ouxgUWja'
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51INbtmEDV1OQTgdxgHzm7ueWWv7IcD9muKeX0zzmkelbghZjGHBMm0tCDVzp8bEgf0QX4AR6wOnXoU64fDEMP17K00ouxgUWja'
 
-return(
-    <StripeCheckout 
-    
-    />
-)
-}
+    onToken = token => {
+        console.log(token);
+        alert('Payment Successful')
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image='http://svgshare.com/i/CUz.svg'
+            description={`Your total is ${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
