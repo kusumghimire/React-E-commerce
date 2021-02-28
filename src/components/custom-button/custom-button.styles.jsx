f368eb0cf675ed7b86e07d5f0c18d9c8068e8d1a
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-02-28 22:38:04 +0545

    getButtonStyles function is created

--- src/components/custom-button/custom-button.styles.jsx
+++ src/components/custom-button/custom-button.styles.jsx
@@ -1,4 +1,41 @@
-import styled, {css} from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const buttonStyles = css`
+background-color: black;
+color: white;
+border: none;
+&:hover{
+    background-color: #357ae8;
+    border:none;
+}
+`;
+
+const invertedButtonStyles = css`
+background-color: white;
+color: black;
+border: 1px solid black;
+&:hover {
+    background-color: black;
+    color: white;
+    border: none;
+}`;
+
+const googleSignInStyles = css`
+background-color: #4285f4;
+color: #fff;
+&:hover {
+    background-color: #357ae8;
+    border: none;
+}
+`;
+
+const getButtonStyles = props => {
+    if (props.googleSignIn) {
+        return googleSignInStyles;
+    }
+
+    return props.inverted ? invertedButtonStyles : buttonStyles;
+}
 
 export const CustomButtonContainer = styled.button`
 min-width: 165px;
@@ -8,19 +45,12 @@ letter-spacing: 0.5px;
 line-height: 50px;
 padding: 0 35px 0 35px;
 font-size: 15px;
-background-color: black;
-color: white;
 text-transform: uppercase;
 font-family: 'Open Sans Condensed';
 font-weight: bolder;
-border: none;
 cursor: pointer;
 display: flex;
 justify-content: center;
-&:hover {
-    background-color: white;
-    color: black;
-    border: 1px solid black;
-}
 
+${getButtonStyles}
 `;
